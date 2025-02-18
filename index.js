1fdd4555fb066283d8d9adcd5f2dd69387fd721d
parukazz <parukazziyi@gmail.com>
2025-02-19 05:15:40 +0700

    basic js

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello, world!");
+
+// Function
+function sayHello(name) {
+  return `Hello, ${name}!`;
+}
+console.log(sayHello("Paruk"));
+
+// Arrow Function
+const sayHello2 = (name) => `Hello, ${name}!`;
+console.log(sayHello2("Paruk"));
